29e42ae2535119a414d720177e39f4fe9a1cef08
Foxtrot007-ai <kaziuk28@gmail.com>
2024-02-17 07:09:45 +0100

    Project initialized

---
+++ TripManager/src/test/java/com/Foxtrot007/TripManager/TripManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Foxtrot007.TripManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
